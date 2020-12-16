95afa1763a83bc2a2f120a3f4a0a2865666da5b5
daniel <daniel2002921@gmail.com>
2020-12-16 15:06:20 +0800

    登入系統以及index.php的position完成

--- login.php
+++ login.php
@@ -9,29 +9,35 @@
 	$sql = "SELECT * FROM AOCI_DB.user_table where account ='".$id."'";
 	
 
-	echo $sql;
+	// echo $sql;
 	
 	$db_result = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 	
-	print_r($db_result);
+	// print_r($db_result);
 
 	
 
 	//echo $rows;
-	echo '<br>';
-	echo "id=".$id;
-	echo '<br>'; 
-		echo "pw=";
-	 echo $pw;
+	// echo '<br>';
+	// echo "id=".$id;
+	// echo '<br>'; 
+	// 	echo "pw=";
+	//  echo $pw;
 	// echo '<br>'; 
 
-	echo $_SESSION['account'];
+	// echo $_SESSION['account'];
 
+
+	$status = array();
+	$status['code'] = 0;
 
 
 	if($db_result == null)
 	{
-	        echo "<script>alert('您尚未註冊。')</script>";       
+	        //echo "<script>alert('您尚未註冊。')</script>";     
+	        // header("Location: indexx.html");  
+			$status['text'] = '帳號不存在，您尚未註冊。';
+
 	} 
 
 
@@ -41,24 +47,36 @@
 	        //將帳號寫入session，方便驗證使用者身份
 	        $_SESSION['account'] = $id;
 			$_SESSION['password'] = $pw;
-	        echo "<script>alert('歡迎".$id."使用系統!')</script>";
+
+			
+	        // echo "<script>alert('歡迎".$id."使用系統!')</script>";
 	        // echo '<meta http-equiv=REFRESH CONTENT=1;url=userpage.php>';
+	        // header("Location: index.php");
 	      
-		   
+		   	$status['text'] = "'歡迎".$id."使用系統!'";
+		   	$status['code'] = 1;
 		   
 		
 	}
 	else if($id == null && $pw == null)
 	{
-		echo "<script>alert('請輸入帳號、密碼。')</script>";
+		//echo "<script>alert('請輸入帳號、密碼。')</script>";
+		// header("Location: indexx.html");
+
+			$status['text'] = '請輸入帳號、密碼。';
 	}
 
 	else if($pw !=$db_result['0']['password'] ){
 
-		echo "<script>alert('密碼輸入錯誤。')</script>";
+		//echo "<script>alert('密碼輸入錯誤。')</script>";
+		// header("Location: indexx.html");
+		$status['text'] = '密碼輸入錯誤。';
 	}
+
 	
- header("Location: index.php");
+	echo json_encode($status);  
+	
+ 
 
 	
 ?>
